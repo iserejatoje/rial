14eae693c384ff5b71eaca37ed8b15c34e081e47
Sergey <serejatoje@gmail.com>
2024-02-23 12:00:36 +0700

    contact page

--- frontend/map.js
+++ frontend/map.js
@@ -50,7 +50,7 @@ function initMap() {
 
 if (document.getElementById('map')) {
     if (document.querySelector('[data-lazy]')) {
-        setTimeout(initMap, 1200)
+        setTimeout(initMap, 1400)
     }
 
     window.addEventListener('scroll', () => {
