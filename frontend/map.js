83dcc0a1f6a45a8671e4581c25b6a2ccce3e6df3
Sergey <serejatoje@gmail.com>
2024-02-23 11:47:30 +0700

    contact page

--- frontend/map.js
+++ frontend/map.js
@@ -24,34 +24,34 @@ function initMap() {
         let script = document.createElement("script")
         script.src = "https://api-maps.yandex.ru/2.1/?load=package.standard&apikey=f44f9a2d-2cef-4d09-a399-6ea9b5cedf55&lang=ru-RU"
         document.getElementsByTagName('body')[0].appendChild(script)
-        // let pin = document.getElementById('map').getAttribute('data-pin')
+        let pin = document.getElementById('map').getAttribute('data-pin')
         script.onload = function () {
-            let coords = [55.809171, 37.611976]
             ymaps.ready(function () {
                 let map = new ymaps.Map('map', {
-                    center: coords,
+                    center: [55.721194, 37.755401],
                     zoom: 14,
                     controls: ['zoomControl']
                 }, {})
                 map.behaviors.disable('scrollZoom')
-
-                // let placemark = new ymaps.Placemark([55.721194, 37.755401], {
-                //     balloonContentHeader: 'Риал Лидер',
-                //     balloonContentBody: 'Москва, ул.Стахановская, 19'
-                // }, {
-                //     iconLayout: 'default#image',
-                //     iconImageHref: pin,
-                //     iconImageSize: [46, 57],
-                //     iconImageOffset: [-23, -57],
-                // })
-                // map.geoObjects.add(placemark)
+                let placemark = new ymaps.Placemark([55.721194, 37.755401], {
+                    balloonContentHeader: 'Риал Лидер',
+                    balloonContentBody: 'Москва, ул.Стахановская, 19'
+                }, {
+                    iconLayout: 'default#image',
+                    iconImageHref: pin,
+                    iconImageSize: [41, 54],
+                    iconImageOffset: [-20, -54],
+                })
+                map.geoObjects.add(placemark)
             })
         }
     }
 }
 
 if (document.getElementById('map')) {
-    initMap()
+    if (document.querySelector('[data-lazy]')) {
+        setTimeout(initMap, 500)
+    }
 
     window.addEventListener('scroll', () => {
         initMap()
